ed0f4940cc41cb30403a46347853071942b7af9a
Kevin Lin <lin.kevin.01@gmail.com>
2014-11-11 01:38:56 -0500

    implemented WeightedRandomModel class

---
+++ RandomLib.Tests/WeightedRandomModelTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RandomLib.Interfaces;
+using RandomLib.Models;
+
+namespace RandomLib.Tests
+{
+    [TestClass]
+    public class WeightedRandomModelTests
+    {
+        public class WeightedRandomString : IWeightedRandomItem
+        {
+            public int Id { get; set; }
+            public int Weight { get; set; }
+            public string Value { get; set; }
+        }
+
+        private readonly string[] values = {"TestValue1", "TestValue2", "TestValue3", "TestValue4"};
+            
+        [TestMethod]
+        public void Test_ZeroWeightItemNeverOccurs()
+        {
+            var items = new List<WeightedRandomString>
+            {
+                new WeightedRandomString() { Value = "Test0", Weight = 0 },
+                new WeightedRandomString() { Value = "Test1", Weight = 1 },
+                new WeightedRandomString() { Value = "Test2", Weight = 2 },
+                new WeightedRandomString() { Value = "Test3", Weight = 3 },
+                new WeightedRandomString() { Value = "Test4", Weight = 4 }
+            };
+
+            var model = new WeightedRandomModel<WeightedRandomString>(items);
+            for (var i = 0; i < 1000; i++)
+            {
+                Assert.AreNotEqual(model.GetRandomItem().Value, "Test0");
+            }
+        }
+    }
+}
--- RandomLib/Models/WeightedRandomModel.cs
+++ RandomLib/Models/WeightedRandomModel.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RandomLib.Interfaces;
 
 namespace RandomLib.Models
 {
-    class WeightedRandomModel
+    public class WeightedRandomModel<T> : IRandomModel<T>
+        where T : IWeightedRandomItem
     {
+        private readonly Random _randomObj;
+        private readonly List<T> _itemList;
+ 
+        public WeightedRandomModel()
+        {
+            _itemList = new List<T>();
+            _randomObj = new Random();
+        }
+
+        public WeightedRandomModel(List<T> items) : this()
+        {
+            _itemList = items;
+        }
+
+        public void AddItem(T item)
+        {
+            _itemList.Add(item);
+        }
+
+        public T GetRandomItem()
+        {
+            var weights = _itemList.Select(i => i.Weight);
+            var gcdOfWeights = GCD(weights);
+
+            var weightedItemList = new List<T>();
+            _itemList.ForEach(i =>
+            {
+                var reducedWeight = i.Weight/gcdOfWeights;
+                for(var j = 0; j < reducedWeight; j++)
+                    weightedItemList.Add(i);
+            });
+
+            var model = new RandomModel<T>(weightedItemList);
+            return model.GetRandomItem();
+        }
+
+        private static int GCD(IEnumerable<int> numbers)
+        {
+            return numbers.Aggregate(GCD);
+        }
+
+        private static int GCD(int a, int b)
+        {
+            return b == 0 ? a : GCD(b, a % b);
+        }
     }
 }
